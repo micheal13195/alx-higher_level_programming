3f6db4936a534bc8673b9dcdcd85660687001381
mickay <michealaderibigbe@gmail.com>
2024-06-04 22:46:41 +0100

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,6 @@
 -- script that creates a table second_table
 CREATE TABLE IF NOT EXIST (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (4, "George", 8);
