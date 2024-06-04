8885aa27ef4f17a0d3115b88874084d1f0c268e0
mickay <michealaderibigbe@gmail.com>
2024-06-04 23:07:39 +0100

    sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
 -- scripts that lists all records of the table second_table
-SELECT * FROM `seond_table`;
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `seond_table`
+ORDER BY `score` DESC;
