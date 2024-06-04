b5ce5656040e1de896a95fa37a369525fbcc1d43
mickay <michealaderibigbe@gmail.com>
2024-06-04 16:00:43 +0100

    sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- script that inserts a new row in the table 'first_table'
-INSERT ('id' 89, 'name' = Best School) 
+-- script that inserts a new row in the table `first_table`
+INSERT INTO `first_table` ('id' 89, 'name' = Best School) 
