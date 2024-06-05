246ab8e2c8a1766a128bab22c6f4673aa7bff23f
mickay <michealaderibigbe@gmail.com>
2024-06-05 12:05:07 +0100

    sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,7 @@
 -- List all records with a score >=10 in the second table
 -- Records are ordered by descending score.
 -- List only scores that are above or equal to 10 in descending order
-SELECT `score >= 10`, `name`
+SELECT `score`, `name`
 FROM `second_table`
-ORDER BY `score >=10` DESC; 
+WHERE `score` >= 10
+ORDER BY `score` DESC; 
