f4f0ec74975cd7aeb268e613e0592fddec67741b
mickay <michealaderibigbe@gmail.com>
2024-06-05 12:27:19 +0100

    sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,3 @@
 -- script that updates the score of Bob to 10 in the second_table
-FROM `second_table`
-WHERE name = `Bob`
-UPDATE `second_table` (`score` 10)
+UPDATE `second_table` (`score` = 10)
+WHERE name = "Bob";
