d4024a9ea732ba8b91ea13395253cccb2ee76b05
mickay <michealaderibigbe@gmail.com>
2024-06-07 15:47:34 +0100

    more sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,4 +3,4 @@
 CREATE DATABASE
 	IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE
-	IF NOT EXISTS `hbtn_0d_usa`.`states` (`id` INT UNIQUE NOT NULL, `name` VARCHAR(256) NOT NULL);
+	IF NOT EXISTS `hbtn_0d_usa`.`states` (PRIMARY KEY(`id`), `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL)
