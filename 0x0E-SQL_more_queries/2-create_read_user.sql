b7f3b9b7536a90af2fc403f6b25983104b357a43
mickay <michealaderibigbe@gmail.com>
2024-06-07 11:43:15 +0100

    more sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,9 @@
 -- Lists that creates the database 'hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE 
 	IF NOT EXISTS 'hbtn_0d_2'
-	IDENTIFIED BY 'user_0d_2_pwd'
-GRANT SELECT PRIVILEGES
-ON *.*
-TO 'user_0d_2'@'hbtn_0d_2';
+CREATE USER
+	IF NOT EXISTS 'user_0d_2'@'localhost'
+	IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT 
+ON *'hbtn_0d_2'.*
+TO 'user_0d_2'@'localhost';
