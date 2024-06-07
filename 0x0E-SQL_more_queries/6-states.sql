546220da8c2cd248800f965eb9a1acc67b3d13a7
mickay <michealaderibigbe@gmail.com>
2024-06-07 13:19:03 +0100

    more sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- scripts that create a database hbtn_0d_usa 
+-- a table inside the data base name state
+CREATE DATABASE
+	IF NOT EXISTS `hbtn_0d_usa`
+CREATE TABLE
+	IF NOT EXISTS `states` (`id` INT UNIQUE NOT NULL, `name` VARCHAR(256) NOT NULL);
