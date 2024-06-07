e383a2afb499d8172b342169583b084b494ad4f1
mickay <michealaderibigbe@gmail.com>
2024-06-07 15:28:46 +0100

    more sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- scripts that create a database hbtn_0d_usa 
 -- a table inside the data base name state
 CREATE DATABASE
-	IF NOT EXISTS `hbtn_0d_usa`
+	IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE
 	IF NOT EXISTS `hbtn_0d_usa`.`states` (`id` INT UNIQUE NOT NULL, `name` VARCHAR(256) NOT NULL);
