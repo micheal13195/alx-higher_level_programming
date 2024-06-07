372ff88b4421c5f64e59721213fe2d2d3bb5899a
mickay <michealaderibigbe@gmail.com>
2024-06-07 21:47:36 +0100

    more sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,5 +2,8 @@
 -- a table inside the data base name state
 CREATE DATABASE
 	IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE
-	IF NOT EXISTS `hbtn_0d_usa`.`states` (PRIMARY KEY(`id`), `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL)
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	PRIMARY KEY(`id`),
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(256) NOT NULL
+)
