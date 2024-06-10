36eec284291ba75d5cf6c7487d8edcfaa3222104
mickay <michealaderibigbe@gmail.com>
2024-06-10 22:15:44 +0100

    sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computes the average score of all records in the table second_table.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
+(base) sheriffquadri@Mickays-MBP 0x0D-SQL_introduction % 
